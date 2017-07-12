43699dad04d6861be27d56047ffbd653125b1e39
eddie <eddy_gg@163.com>
2017-07-12 18:36:59 +0800

    js

--- js/service.js
+++ js/service.js
@@ -1,7 +1,19 @@
 let http = require("http");
+let url = require("url");
 
-http.createServer(function(request,response){
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("hi,eddie");
-    response.end();
-}).listen(9999);
+function start(route) {
+    function onRequest(request, response) {
+        let pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(pathname);
+
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write("hi,eddie");
+        response.end();
+    }
+    http.createServer(onRequest).listen(9999);
+    console.log("server started");
+}
+
+exports.start = start;
